f9402fe8a53c6921a89b9ba407e7343875227c75
Jiahui Zou <60097246+jhz940303@users.noreply.github.com>
2023-03-07 16:40:57 -0800

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,171 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+training_set_path = "/Users/jiahuizou/Desktop/6140/project4/greek_train"
+
+testing_set_path = "/Users/jiahuizou/Desktop/6140/project4/greek_test"
+
+model_path = "/Users/jiahuizou/Desktop/6140/project4/model.pth"
+
+def Greek_Letter():
+    # greek data set transform
+    class GreekTransform:
+        def __init__(self):
+            pass
+
+        def __call__(self, x):
+            x = torchvision.transforms.functional.rgb_to_grayscale(x)
+            x = torchvision.transforms.functional.affine(x, 0, (0, 0), 36 / 128, 0)
+            x = torchvision.transforms.functional.center_crop(x, (28, 28))
+            return torchvision.transforms.functional.invert(x)
+
+    n_epochs = 20
+    batch_size_train = 1
+    batch_size_test = 9
+    learning_rate = 0.01
+    momentum = 0.5
+    log_interval = 10
+
+    random_seed = 1
+    torch.backends.cudnn.enabled = False
+    torch.manual_seed(random_seed)
+
+    # DataLoader for the Greek data set
+    train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.ImageFolder(training_set_path,
+                                         transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+                                                                                   GreekTransform(),
+                                                                                   torchvision.transforms.Normalize(
+                                                                                       (0.1307,), (0.3081,))])),
+        batch_size=batch_size_train,
+        shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.ImageFolder(testing_set_path,
+                                         transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+                                                                                   GreekTransform(),
+                                                                                   torchvision.transforms.Normalize(
+                                                                                       (0.1307,), (0.3081,))])),
+        batch_size=batch_size_test,
+        shuffle=True)
+
+    examples = enumerate(test_loader)
+    batch_idx, (example_data, example_targets) = next(examples)
+
+    print(example_data.shape)
+
+
+    class Net(nn.Module):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.conv1 = nn.Conv2d(1, 10, kernel_size=3)  # 28-3+1 = 26 * 26 * 10
+            self.conv2 = nn.Conv2d(10, 20, kernel_size=2)  # 13-2+1 = 12 * 12 * 20
+            self.conv3 = nn.Conv2d(20, 30, kernel_size=3)  # 6-3+1 = 4*4*30
+            self.conv3_drop = nn.Dropout2d()
+            self.fc1 = nn.Linear(480, 50) # 4*4*30
+            self.fc2 = nn.Linear(50, 10)
+
+        def forward(self, x):
+            x = F.relu(F.max_pool2d(self.conv1(x), 2))  # 26/2 = 13 * 13 * 10
+            x = F.relu(F.max_pool2d(self.conv2(x), 2))  # 12/2 = 6 * 6 * 20
+            x = F.relu(self.conv3_drop(self.conv3(x)))  # 4*4*30
+            x = x.view(-1, 480)
+            x = F.relu(self.fc1(x))
+            x = F.dropout(x, training=self.training)
+            x = self.fc2(x)
+            return F.log_softmax(x)
+
+
+    network = Net()
+
+    network.load_state_dict(torch.load(model_path))
+    optimizer = optim.SGD(network.parameters(), lr=learning_rate,
+                          momentum=momentum)
+    # freezes the parameters for the whole network
+    for param in network.parameters():
+        param.requires_grad = False
+
+    train_losses = []
+    train_counter = []
+    test_losses = []
+    test_counter = [i * len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+    def train(epoch):
+        network.train()
+        print("Train Epoch:", epoch)
+        for batch_idx, (data, target) in enumerate(train_loader):
+            optimizer.zero_grad()
+            output = network(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % log_interval == 0:
+                # print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                #     epoch, batch_idx * len(data), len(train_loader.dataset),
+                #            100. * batch_idx / len(train_loader), loss.item()))
+
+                train_losses.append(loss.item())
+                train_counter.append(
+                    (batch_idx * 64) + ((epoch - 1) * len(train_loader.dataset)))
+                torch.save(network.state_dict(), '/Users/jiahuizou/Desktop/6140/project4/model.pth')
+                torch.save(optimizer.state_dict(), '/Users/jiahuizou/Desktop/6140/project4/optimizer.pth')
+
+    def test():
+        network.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                output1 = network(data)
+                test_loss += F.nll_loss(output1, target, size_average=False).item()
+                pred = output1.data.max(1, keepdim=True)[1]
+                correct += pred.eq(target.data.view_as(pred)).sum()
+        test_loss /= len(test_loader.dataset)
+        test_losses.append(test_loss)
+        print('Test set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+
+    test()
+    for epoch in range(1, n_epochs + 1):
+        train(epoch)
+        test()
+
+    fig = plt.figure()
+    plt.plot(train_counter, train_losses, color='blue')
+    plt.scatter(test_counter, test_losses, color='red')
+    plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
+    plt.xlabel('number of training examples seen')
+    plt.ylabel('negative log likelihood loss')
+    plt.show()
+
+    with torch.no_grad():
+        output = network(example_data)
+
+    fig = plt.figure()
+    for i in range(6):
+        plt.subplot(2, 3, i + 1)
+        plt.tight_layout()
+        plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+        plt.title("Prediction: {}".format(
+            output.data.max(1, keepdim=True)[1][i].item()))
+        plt.xticks([])
+        plt.yticks([])
+    plt.show()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    Greek_Letter()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
