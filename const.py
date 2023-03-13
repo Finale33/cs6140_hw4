00013bcac424f441f540763719c8eb1005192d97
Finale33 <yihan.xusichuan@gmail.com>
2023-03-13 14:41:34 -0700

    task 4 structure

--- const.py
+++ const.py
@@ -15,3 +15,7 @@ TASK1_OPTIMIZER_PATH = f"{FILE_ROOT}/optimizer.pth"
 TASK2_OPTIMIZER_PATH = f"{FILE_ROOT}/optimizer_task2.pth"
 
 TASK3_OPTIMIZER_PATH = f"{FILE_ROOT}/optimizer_task3.pth"
+
+HEART_DISEASE_TRAIN_PATH = f"{FILE_ROOT}/cs6140_hw4/dataset/heart_train_718.csv"
+
+HEART_DISEASE_TEST_PATH = f"{FILE_ROOT}/cs6140_hw4/dataset/heart_test_200.csv"
---
+++ task_4.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+import tensorflow as tf
+from tensorflow import keras
+import const
+
+
+def normalize_dataset(df):
+    # Encode categorical variables
+    encoder = LabelEncoder()
+    df['sex'] = encoder.fit_transform(df['sex'])
+    df['chestpaintype'] = encoder.fit_transform(df['chestpaintype'])
+    df['fastingBS'] = encoder.fit_transform(df['fastingBS'])
+    df['RestingECG'] = encoder.fit_transform(df['RestingECG'])
+    df['ExerciseAngina'] = encoder.fit_transform(df['ExerciseAngina'])
+    df['ST_Slope'] = encoder.fit_transform(df['ST_Slope'])
+
+    # Normalize numerical variables
+    scaler = StandardScaler()
+    df[['age', 'restingBP', 'cholesterol', 'MaxHR', 'Oldpeak']] = scaler.fit_transform(
+        df[['age', 'restingBP', 'cholesterol', 'MaxHR', 'Oldpeak']])
+
+
+def simple_architecture(X_train, y_train, X_test, y_test):
+    # Define the neural network architecture
+    model = keras.Sequential([
+        keras.layers.Dense(32, activation='relu', input_shape=(11,)),
+        keras.layers.Dense(16, activation='relu'),
+        keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    # Compile the model
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    # Train the model
+    model.fit(X_train, y_train, epochs=100, batch_size=16, verbose=1)
+
+    # Evaluate the model on the test set
+    score = model.evaluate(X_test, y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+
+if __name__ == '__main__':
+    # Load heart disease dataset
+    training_df = pd.read_csv(const.TRAININT_SET_PATH)
+    test_df = pd.read_csv(const.TESTING_SET_PATH)
+
+    normalize_dataset(training_df)
+    normalize_dataset(test_df)
+
+    X_train = training_df.drop(['HeartDisease'], axis=1)
+    y_train = training_df['HeartDisease']
+    X_test = test_df.drop(['HeartDisease'], axis=1)
+    y_test = test_df['HeartDisease']
+
+    simple_architecture(X_train, y_train, X_test, y_test)
